006d2c09a6a606f1189c6b01a82463f9dc727ead
GabrielKishida <gabriel.kishida@usp.br>
2024-02-26 17:40:34 -0300

    :construction: Capture attack command

--- Assets/Custom/Scripts/PlayerController.cs
+++ Assets/Custom/Scripts/PlayerController.cs
@@ -12,12 +12,12 @@ public class PlayerController : MonoBehaviour
     public float gravity = 20.0f; // Gravity force
 
     private Vector2 currentSpeed = Vector2.zero;
-
     private CharacterController controller;
     private Vector3 moveDirection = Vector3.zero;
     public PlayerInputActions playerControls;
     private InputAction move;
     private InputAction roll;
+    private InputAction attack;
 
     private void Awake()
     {
@@ -28,8 +28,10 @@ public class PlayerController : MonoBehaviour
     {
         move = playerControls.Player.Move;
         roll = playerControls.Player.Roll;
+        attack = playerControls.Player.Attack;
         move.Enable();
         roll.Enable();
+        attack.Enable();
 
     }
 
@@ -37,6 +39,7 @@ public class PlayerController : MonoBehaviour
     {
         move.Disable();
         roll.Disable();
+        attack.Disable();
     }
 
     void Start()
@@ -46,6 +49,12 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        MoveCharacter();
+        Attack();
+    }
+
+    private void MoveCharacter()
+    {
         if (currentSpeed.magnitude < minimumSpeed)
         {
             currentSpeed = Vector2.zero;
@@ -54,7 +63,6 @@ public class PlayerController : MonoBehaviour
         {
             currentSpeed = currentSpeed - currentSpeed * dragCoefficient;
         }
-
         Vector2 moveInput = move.ReadValue<Vector2>();
         if (moveInput != Vector2.zero)
         {
@@ -64,17 +72,21 @@ public class PlayerController : MonoBehaviour
                 currentSpeed = Vector3.ClampMagnitude(currentSpeed, maxSpeed);
             }
         }
-
         moveDirection = new Vector3(currentSpeed.x, 0.0f, currentSpeed.y);
         if (moveDirection != Vector3.zero)
         {
             Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
             transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
         }
-
-
         moveDirection.y -= gravity;
-
         controller.Move(moveDirection * Time.deltaTime);
+    }
+
+    private void Attack()
+    {
+        if (attack.ReadValue<float>() == 1)
+        {
+            Debug.Log("Attack");
+        }
     }
 }
